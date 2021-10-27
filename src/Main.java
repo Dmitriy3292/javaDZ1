f896be2623ef5e001c67acae137b57322fcdfa5c
Dmitriy3292 <mr.diy@yandex.ru>
2021-10-27 17:46:50 +0400

    report

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int flowingBalance = 2000000000;
         int transh = 500000000;
-        int totalSum = flowingBalance * transh;
+        int totalSum = flowingBalance + transh;
         System.out.println(totalSum);
     }
 }
